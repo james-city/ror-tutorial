9d0cd0e9d90bef9d234c58439d9ae19623bccb23
James City <j@james.city>
2025-01-11 22:40:47 -0500

    Use  shortcut for products CRUD routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,16 +14,5 @@ Rails.application.routes.draw do
 end
 
 Rails.application.routes.draw do
-  get "/products", to: "products#index"
-
-  get "/products/new", to: "products#new"
-  post "/products", to: "products#create"
-
-  get "/products/:id" to: "products#show"
-
-  get "/products/:id/edit", to "products#edit"
-  patch "/products/:id", to: "products#update"
-  put "/products/:id", to: "products#update"
-
-  delete "/products/:id", to: "products#destroy"
+  resources :products
 end

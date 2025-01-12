758f7fbb657f79635da110544131be2690fbc72b
James City <j@james.city>
2025-01-12 01:30:24 -0500

    Add authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -14,6 +16,8 @@ Rails.application.routes.draw do
 end
 
 Rails.application.routes.draw do
+  resource :session
+  resources :passwords, param: :token
   resources :products
   root "products#index"
 end

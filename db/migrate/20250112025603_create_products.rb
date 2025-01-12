2e88fc2a02d90965179397ace6c31e6bac43f47b
James City <j@james.city>
2025-01-11 22:39:59 -0500

    Create Products model

--- db/migrate/20250112025603_create_products.rb
+++ db/migrate/20250112025603_create_products.rb
@@ -1,8 +1,12 @@
 class CreateProducts < ActiveRecord::Migration[8.0]
   def change
+    # create_table block defines which columns and types should be defined in
+    # the database table
     create_table :products do |t|
+      # make a column called |name| of type |string|
       t.string :name
 
+      # make a created_time:datetime and updated_at:datetime column
       t.timestamps
     end
   end
